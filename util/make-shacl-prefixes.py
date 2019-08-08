d63b32c4e7c14cefd95757ece969ddace0c6f1fe
cmungall <cjm@berkeleybop.org>
2019-08-07 21:16:18 -0700

    adding docs. Ensuring prefixes built with "all" target

--- util/make-shacl-prefixes.py
+++ util/make-shacl-prefixes.py
@@ -8,6 +8,26 @@ from argparse import ArgumentParser
 
 
 def main(args):
+  """
+  Takes ontologies.yml file and makes a triple file with SHACL prefixes.
+
+  For example, for uberon it will generate:
+
+      [ sh:prefix "UBERON" ; sh:namespace "http://purl.obolibrary.org/obo/UBERON_"]
+
+  We always assume the CURIE prefix is uppercase, unless 'preferred_prefix' is specified
+  (for mixed-case prefixes, e.g. FBbt)
+
+  This can be useful for converting an OBO class PURL to a prefix without assumption-embedding string conversions.
+  It can be used to interconvert PURLs to CURIEs.
+
+  Note that while prefixes can sometimes be seen in RDF files, this is part of the syntax and not part of the data,
+  the prefixes are expanded at parse time. The obo_prefixes.ttl file makes these explicit.
+
+  We use the SHACL vocabulary since it provides convenient predicates for putting prefixes in the domain of discourse;
+  however, it does not entail any use of SHACL
+
+  """
   parser = ArgumentParser(description='''
   Takes ontologies.yml file and makes a triple file with shacl prefixes''')
   parser.add_argument('input')
