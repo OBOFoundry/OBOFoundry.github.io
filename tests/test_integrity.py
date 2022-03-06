ecb030dd286a6fb27440c9196a1681a5fdad5829
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-03-06 18:51:51 +0100

    Add tests

---
+++ tests/test_integrity.py
@@ -0,0 +1,50 @@
+"""Test data integrity, beyond what's possible with the JSON schema."""
+
+import unittest
+from io import StringIO
+from pathlib import Path
+
+import yaml
+
+HERE = Path(__file__).parent.resolve()
+ROOT = HERE.parent
+ONTOLOGY_DIRECTORY = ROOT.joinpath("ontology").resolve()
+
+
+def get_data():
+    ontologies = {}
+    for path in ONTOLOGY_DIRECTORY.glob("*.md"):
+        with open(path) as file:
+            lines = [line.rstrip("\n") for line in file]
+
+        assert lines[0] == "---"
+        idx = min(i for i, line in enumerate(lines[1:], start=1) if line == "---")
+
+        # Load the data like it is YAML
+        data = yaml.safe_load(StringIO("\n".join(lines[1:idx])))
+        ontologies[data["id"]] = data
+    return ontologies
+
+
+class TestIntegrity(unittest.TestCase):
+    """Test case for data integrity."""
+
+    def setUp(self) -> None:
+        """Set up the test case."""
+        self.ontologies = get_data()
+
+    def test_dependencies(self):
+        """Test dependencies are valid OBO Foundry ontologies."""
+        for ontology, data in self.ontologies.items():
+            dependencies = data.get("dependencies")
+            if not dependencies:
+                continue
+            with self.subTest(ontology=ontology):
+                for i, dependency in enumerate(dependencies):
+                    # Check that the ID is a valid OBO ontology prefix
+                    dep_id = dependency["id"]
+                    self.assertIn(
+                        dep_id,
+                        set(self.ontologies),
+                        msg=f"Ontology {ontology} has invalid dependency at index {i}: {dep_id}",
+                    )
