fd96a40188a8e5c145f7f5f6743f78608f7d65bb
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-04-21 10:29:35 +0200

    Loosen publication validation to allow Zenodo, arXiv, bioRxiv, and medRxiv

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -9,7 +9,11 @@ HERE = Path(__file__).parent.resolve()
 ROOT = HERE.parent
 ONTOLOGY_DIRECTORY = ROOT.joinpath("ontology").resolve()
 
-uri_prefix = "https://www.ncbi.nlm.nih.gov/pubmed/"
+PUBMED_PREFIX = "https://www.ncbi.nlm.nih.gov/pubmed/"
+ARXIV_PREFIX = "https://arxiv.org/abs/"
+BIORXIV_PREFIX = "https://www.biorxiv.org/content/"
+MEDRXIV_PREFIX = "https://www.medrxiv.org/content/"
+ZENODO_PREFIX = "https://zenodo.org/record/"
 
 
 def get_data():
@@ -89,5 +93,21 @@ class TestIntegrity(unittest.TestCase):
         identifier = publication["id"]
         self.assertIsInstance(identifier, str)
         self.assertFalse(identifier.endswith("/"))
-        self.assertTrue(identifier.startswith(uri_prefix), msg=msg)
-        self.assertTrue(identifier[len(uri_prefix):].isnumeric())
+
+        is_pubmed = identifier.startswith(PUBMED_PREFIX) and identifier[len(PUBMED_PREFIX):].isnumeric()
+        is_zenodo = identifier.startswith(ZENODO_PREFIX) and identifier[len(ZENODO_PREFIX):].isnumeric()
+        # TODO add regular expression validation for arXiv, bioRxiv, medRxiv
+        is_arxiv = identifier.startswith(ARXIV_PREFIX)
+        is_biorxiv = identifier.startswith(BIORXIV_PREFIX)
+        is_medrxiv = identifier.startswith(MEDRXIV)
+
+        self.assertTrue(
+            any((
+                is_pubmed,
+                is_zenodo,
+                is_arxiv,
+                is_biorxiv,
+                is_medrxiv,
+            )),
+            msg=msg,
+        )
