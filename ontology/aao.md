c748947e82dd874c8b1578caa66985087dd6b89b
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-07-06 11:24:01 -0400

    Modify dumping procedure

--- ontology/aao.md
+++ ontology/aao.md
@@ -1,17 +1,17 @@
 ---
-activity_status: inactive
+layout: ontology_detail
+id: aao
+title: Amphibian gross anatomy
 contact:
   email: david.c.blackburn@gmail.com
   label: David Blackburn
 homepage: http://github.com/seger/aao
-id: aao
 is_obsolete: true
-layout: ontology_detail
 replaced_by: uberon
 taxon:
   id: NCBITaxon:8292
   label: Amphibia
-title: Amphibian gross anatomy
+activity_status: inactive
 ---
 
 A structured controlled vocabulary of the anatomy of Amphibians. Note that AAO is currently being integrated into Uberon.
--- util/standardize_metadata.py
+++ util/standardize_metadata.py
@@ -9,13 +9,58 @@ Author: `Charles Tapley Hoyt <https://cthoyt.com>`_.
 
 import pathlib
 from io import StringIO
-from typing import Union
 
 import click
 import yaml
+from yaml import MappingNode, SafeDumper, ScalarNode
 
 HERE = pathlib.Path(__file__).parent.resolve()
 ONTOLOGY_DIRECTORY = HERE.parent.joinpath("ontology").resolve()
+
+
+def sort_key(kv):
+    k, v = kv
+    if k == "layout":
+        return 1, k
+    elif k == "id":
+        return 2, k
+    elif k == "title":
+        return 3, k
+    elif k == "activity_status":
+        return 5, k
+    else:
+        return 4, k
+
+
+class ModifiedDumper(SafeDumper):
+    def represent_mapping(self, tag, mapping, flow_style=None):
+        value = []
+        node = MappingNode(tag, value, flow_style=flow_style)
+        if self.alias_key is not None:
+            self.represented_objects[self.alias_key] = node
+        best_style = True
+        if hasattr(mapping, "items"):
+            mapping = list(mapping.items())
+            if self.sort_keys:
+                try:
+                    # all code is directly from the base class except this sorted
+                    mapping = sorted(mapping, key=sort_key)
+                except TypeError:
+                    pass
+        for item_key, item_value in mapping:
+            node_key = self.represent_data(item_key)
+            node_value = self.represent_data(item_value)
+            if not (isinstance(node_key, ScalarNode) and not node_key.style):
+                best_style = False
+            if not (isinstance(node_value, ScalarNode) and not node_value.style):
+                best_style = False
+            value.append((node_key, node_value))
+        if flow_style is None:
+            if self.default_flow_style is not None:
+                node.flow_style = self.default_flow_style
+            else:
+                node.flow_style = best_style
+        return node
 
 
 def update_markdown(path: pathlib.Path) -> None:
@@ -28,8 +73,9 @@ def update_markdown(path: pathlib.Path) -> None:
 
     # Load the data like it is YAML
     data = yaml.safe_load(StringIO("\n".join(lines[1:idx])))
-    dumped = yaml.safe_dump(
+    dumped = yaml.dump(
         data,
+        Dumper=ModifiedDumper,
         sort_keys=True,
         explicit_end=False,
         width=float("inf"),
@@ -39,7 +85,7 @@ def update_markdown(path: pathlib.Path) -> None:
         print("---", file=file)
         print(dumped, file=file)
         print("---", file=file)
-        for line in lines[idx + 1 :]:
+        for line in lines[idx + 1:]:
             print(line, file=file)
 
 
