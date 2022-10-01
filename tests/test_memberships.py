f54608b628cd6e77f68931cbe9cad020d2511e77
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-01 17:21:22 +0200

    Structure membership information (#2117)
    * Start updating membership storage
    
    * Add unit tests
    
    * Update TechnicalWG.md
    
    * Get remaining data
    
    * Add technical working group structured data
    
    * Add editorial
    
    * Add links and update pages
    
    * Update OperationsCommittee.md
    
    * Update
    
    * ORCID required
    
    * Add ORCIDs for alumni
    
    Still need:
    - Eric Douglass
    - Gareth Owen
    - Carlo Tornial
    
    * Update Membership.md
    
    * Merge data based on Nico's request
    
    * Split out country - show massive USA centric nature of OBO ops
    
    * Add sort by name
    
    * Standardize sort order of operations.yml
    
    * Lint
    
    * Update test_memberships.py
    
    * Update obo-test.yml

---
+++ tests/test_memberships.py
@@ -0,0 +1,42 @@
+"""Tests for working group membership data."""
+
+import unittest
+from collections import Counter
+from pathlib import Path
+from typing import List, Literal
+
+import yaml
+from pydantic import BaseModel
+
+HERE = Path(__file__).parent.resolve()
+ROOT = HERE.parent.resolve()
+DATA = ROOT.joinpath("_data")
+
+
+class Member(BaseModel):
+    """Representation of a member in a working group."""
+
+    name: str
+    orcid: str
+    affiliation: str
+    country: str
+    groups: List[Literal["editorial", "outreach", "technical"]]
+
+
+class Group(BaseModel):
+    """Representation of a working group."""
+
+    members: List[Member]
+
+
+class TestMembershipData(unittest.TestCase):
+    """Test membership data."""
+
+    def test_data(self):
+        """Test the working group data is clean."""
+        path = DATA.joinpath("operations").with_suffix(".yml")
+        res = Group.parse_obj(yaml.safe_load(path.read_text()))
+        self.assertIsNotNone(res)
+        counter = Counter(member.orcid for member in res.members if member.orcid)
+        counter = {orcid for orcid, count in counter.items() if count > 1}
+        self.assertEqual(0, len(counter), msg=f"Duplicate: {counter}")
